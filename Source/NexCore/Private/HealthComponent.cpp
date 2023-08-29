71446ffbde74ec51a7bf6e448e8a0be9a0abcf0d
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-08-29 10:04:27 +0200

    Added level reset and player health component

---
+++ Source/NexCore/Private/HealthComponent.cpp
@@ -0,0 +1,25 @@
+﻿#include "HealthComponent.h"
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UHealthComponent::OnDamageTaken_Implementation()
+{
+}
+
+void UHealthComponent::OnDeath_Implementation()
+{
+}
+
+void UHealthComponent::TakeDamage()
+{
+	--Health;
+	OnDamageTaken();
+	
+	if(Health <= 0)
+	{
+		OnDeath();
+	}
+}
---
+++ Source/NexCore/Public/HealthComponent.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable, BlueprintType)
+class NEXCORE_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHealthComponent();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health Component")
+	int32 Health = 1;
+
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage();
+	
+protected:
+	
+	UFUNCTION(BlueprintNativeEvent)
+	void OnDamageTaken();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnDeath();
+};
