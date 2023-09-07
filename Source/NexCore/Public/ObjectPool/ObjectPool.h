9f019bd57d1d593229dcc9fe645c7149f0bf9b27
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-09-07 15:45:58 +0200

    Added initial wave manager

--- Source/NexCore/Public/ObjectPool/ObjectPool.h
+++ Source/NexCore/Public/ObjectPool/ObjectPool.h
@@ -20,7 +20,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Object Pool")
 	APooledObject* SpawnPooledObject();
 
-	UPROPERTY(EditAnywhere, Category = "Object Pool")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Object Pool")
 	TSubclassOf<class APooledObject> PooledObjectSubclass;
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Object Pool")
